518703941d862c72d1215e1abc0c91a8e09054a5
TobyGe <gyq1101@gmail.com>
2026-02-13 11:31:02 -0500

    Update GitHub Pages to publish docs landing page

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? '/GuardClaw/' : '/',
   server: {
     port: 5173,
     proxy: {
